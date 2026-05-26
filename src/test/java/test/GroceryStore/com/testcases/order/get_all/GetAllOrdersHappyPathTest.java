81ca21ddd53320873c37b861abcb62e9f931277b
OmarGamal <109871074+OmarGamall@users.noreply.github.com>
2026-05-26 15:18:00 +0300

    add Product test cases documentation

--- src/test/java/test/GroceryStore/com/testcases/order/get_all/GetAllOrdersHappyPathTest.java
+++ src/test/java/test/GroceryStore/com/testcases/order/get_all/GetAllOrdersHappyPathTest.java
@@ -3,6 +3,7 @@ package test.GroceryStore.com.testcases.order.get_all;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import test.GroceryStore.com.apis.OrdersApi;
+import test.GroceryStore.com.models.cart.CartItem;
 import test.GroceryStore.com.models.order.Order;
 import test.GroceryStore.com.models.order.OrderRequest;
 import test.GroceryStore.com.models.order.OrderResponse;
@@ -16,13 +17,14 @@ import static org.testng.Assert.*;
 public class GetAllOrdersHappyPathTest extends BaseTest {
 
     @Test
-    public void testGetAllOrdersSuccessfully() {
+    public void testGetAllOrdersForCustomerSuccessfully() {
         // Arrange - Ensure there is at least one order
         String cartId = CartSteps.createCartAndGetId();
-        Product product = ProductService.getRandomAvailableProduct();
-        CartSteps.addItemToCartAndGetResponse(cartId, product.getId(), 1);
+        CartItem cartItem = CartSteps.addRandomItemToCart(cartId);
+        String customerName = FAKER.name().fullName();
+        String comment = FAKER.lorem().sentence();
 
-        OrderRequest orderRequest = new OrderRequest(cartId, "Omar GetAll", "Urgent delivery");
+        OrderRequest orderRequest = new OrderRequest(cartId, customerName, comment);
         Response createResponse = OrdersApi.createOrder(getToken(), orderRequest);
         assertEquals(createResponse.getStatusCode(), 201, "Expected 201 status code for order creation");
         String orderId = createResponse.as(OrderResponse.class).getOrderId();
@@ -41,13 +43,13 @@ public class GetAllOrdersHappyPathTest extends BaseTest {
         for (Order order : orders) {
             if (orderId.equals(order.getId())) {
                 orderFound = true;
-                assertEquals(order.getCustomerName(), "Omar GetAll", "Customer name mismatch in listed order");
+                assertEquals(order.getCustomerName(), customerName, "Customer name mismatch in listed order");
+                assertEquals(order.getComment(), comment, "Comment mismatch in listed order");
+                assertEquals(order.getItems().get(0).getProductId(), cartItem.getProductId(), "Product ID mismatch in listed order");
+                assertEquals(order.getItems().get(0).getQuantity(), cartItem.getQuantity(), "Product quantity mismatch in listed order");
                 break;
             }
         }
         assertTrue(orderFound, "Expected created order to be found in the orders list");
-
-        // Clean up
-        OrdersApi.deleteOrder(getToken(), orderId);
     }
 }
